9a280537f15e4f81abe2278af92d58fef463db7d
TehreemKhan <tehreem93@yahoo.com>
2025-12-05 20:38:43 +0500

    Initial application commit

---
+++ src/main/java/com/example/demo/DemoSpringProject2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringProject2Application.class, args);
+	}
+
+}
